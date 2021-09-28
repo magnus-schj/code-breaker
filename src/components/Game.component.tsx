fb81633a0fa8df99e56d456031d5e10ab697ff6a
Magnus <beachmagnus+git@gmail.com>
2021-09-28 19:34:40 +0200

    pegholders made

--- src/components/Game.component.tsx
+++ src/components/Game.component.tsx
@@ -5,6 +5,7 @@ import {
   Droppable,
   DropResult,
 } from "react-beautiful-dnd";
+import { PegHolder } from "../interfaces";
 import ColourContainer from "./ColourContainer.component";
 const Game: FC = () => {
   const initialState = {
@@ -13,11 +14,38 @@ const Game: FC = () => {
     blue: { id: "blue", hex: "#0000ff" },
   };
 
+  const initialPegs: PegHolder[] = [
+    { colour: null, id: "holder-0" },
+    { colour: null, id: "holder-1" },
+    { colour: null, id: "holder-2" },
+  ];
   const [colours, setColours] = useState(initialState);
-  console.log(Object.entries(colours));
+  const [pegHolders, setPegHolders] = useState(initialPegs);
   return (
     <div className="game">
       <DragDropContext onDragEnd={(result) => console.log("result:", result)}>
+        <div className="inserting-row">
+          {pegHolders.map(({ colour, id }) => {
+            return (
+              <div className="peg-holder" key={id}>
+                <Droppable droppableId={id}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        ref={provided.innerRef}
+                        style={{
+                          background: colour ? colour : "grey",
+                          height: "100%",
+                          width: "100%",
+                        }}
+                      ></div>
+                    );
+                  }}
+                </Droppable>
+              </div>
+            );
+          })}
+        </div>
         <div className="pickable-pegs">
           {Object.entries(colours).map(([id, colour]) => {
             return <ColourContainer id={id} colour={colour} key={id} />;
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,3 +2,8 @@ export interface Colour {
   id: string;
   hex: string;
 }
+
+export interface PegHolder {
+  colour: string | null;
+  id: string;
+}
