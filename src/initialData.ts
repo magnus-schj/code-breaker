168dfdd6b17df6205691d93479c8b79faa69743f
Magnus <beachmagnus+git@gmail.com>
2021-10-07 13:56:07 +0200

    first commit

---
+++ src/initialData.ts
@@ -0,0 +1,10 @@
+import { ColourData } from "./interfaces";
+
+export const colourData: ColourData = {
+  redOrigin: {
+    hsl: "hsl(356, 100%, 41%)",
+  },
+  blueOrigin: {
+    hsl: "hsl(226, 65%, 35%)",
+  },
+};
