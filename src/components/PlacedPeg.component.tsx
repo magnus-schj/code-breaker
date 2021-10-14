6720fcc3961e3870c4662d3e7cb4092cfb5d5ac6
Magnus <beachmagnus+git@gmail.com>
2021-10-14 23:52:42 +0200

    trash icon appears on hover

--- src/components/PlacedPeg.component.tsx
+++ src/components/PlacedPeg.component.tsx
@@ -1,19 +1,30 @@
+import React, { FC, useState } from "react";
+
 import { ButtonBase } from "@material-ui/core";
-import React, { FC } from "react";
+import { Delete } from "@material-ui/icons";
 
 interface Props {
   hsl: string;
 }
 
 const PlacedPeg: FC<Props> = ({ hsl }) => {
+  // delete icon focused or not
+  const [hover, setHover] = useState(false);
   return (
-    <ButtonBase
-      style={{
-        height: "3rem",
-        width: "3rem",
-        background: hsl,
-      }}
-    ></ButtonBase>
+    <div
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <ButtonBase
+        style={{
+          height: "3rem",
+          width: "3rem",
+          background: hsl,
+        }}
+      >
+        <Delete style={{ display: hover ? "block" : "none" }} />
+      </ButtonBase>
+    </div>
   );
 };
 
