d0977dfe1a25fa1c35cc3da1dc0cedbf6873b32c
Magnus <beachmagnus+git@gmail.com>
2021-10-14 13:45:05 +0200

    partly fixed peginputs

--- src/components/Game.component.tsx
+++ src/components/Game.component.tsx
@@ -1,55 +1,54 @@
-import React, { FC, useState } from "react";
-import {
-  DragDropContext,
-  Draggable,
-  Droppable,
-  DropResult,
-} from "react-beautiful-dnd";
-import { Colours, PegHolder, PegInputs } from "../interfaces";
+import { FC, useState } from "react";
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
+import { colourData, pegInputsData } from "../initialData";
+import { ColourData, PegInputs, PegInputType } from "../interfaces";
 import ColourContainer from "./ColourContainer.component";
-import PegInput from "./PegInput.component";
 const Game: FC = () => {
-  const initialState: Colours = {
-    pickRed: { id: "pickRed", hsl: "hsl(360,100%,50%)" },
-    pickGreen: { id: "pickGreen", hsl: "hsl(240,100%,50%)" },
-    pickBlue: { id: "pickBlue", hsl: "hsl(129,100%,50%)" },
-  };
+  const [colours, setColours] = useState<ColourData>(colourData);
+  const [pegInputs, setPegInputs] = useState<PegInputs>(pegInputsData);
 
-  const initialPegs: PegInputs = {
-    one: { colour: null },
-    two: { colour: null },
-    three: { colour: null },
-  };
-  const [colours, setColours] = useState<Colours>(initialState);
-  const [pegInputs, setPegInputs] = useState<PegInputs>(initialPegs);
-
-  const onDragEnd = (result: DropResult, pegInputs: any, setPegInputs: any) => {
+  const onDragEnd = (
+    result: DropResult,
+    setInputs: React.Dispatch<React.SetStateAction<PegInputs>>
+  ) => {
     if (!result.destination) return;
     const { source, destination } = result;
-    if (destination.droppableId.includes("pick")) return;
-
-    const pegDest = pegInputs[destination.droppableId];
-    setPegInputs({
-      ...pegInputs,
-      [destination.droppableId]: {
-        ...pegDest,
-        colour: colours[source.droppableId].hsl,
-      },
-    });
+    const newColourOrigin = { ...colours[source.droppableId] };
+    const newColourDest: PegInputType = {
+      ...pegInputs[destination.droppableId],
+    };
+    newColourDest.peg.length = 0;
+    newColourDest.peg.push(newColourOrigin);
+    setInputs({ ...pegInputs, [newColourDest.id]: newColourDest });
   };
   return (
     <div className="game">
-      <DragDropContext
-        onDragEnd={(result) => onDragEnd(result, pegInputs, setPegInputs)}
-      >
-        <div className="inserting-row">
-          {Object.entries(pegInputs).map(([id, { colour }]) => {
-            return <PegInput key={id} colour={colour} id={id} />;
-          })}
+      <DragDropContext onDragEnd={(result) => onDragEnd(result, setPegInputs)}>
+        <div className="peg-inputs">
+          {Object.entries(pegInputs).map(([id, data]) => (
+            <div
+              key={id}
+              style={{
+                padding: "3rem",
+                border: "1px solid black",
+              }}
+            >
+              <Droppable droppableId={id}>
+                {(provided, snapshot) => (
+                  <div ref={provided.innerRef}>
+                    {data.peg.map((peg) => (
+                      <h1>{peg.hsl}</h1>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </div>
+          ))}
         </div>
         <div className="pickable-pegs">
-          {Object.entries(colours).map(([id, colour]) => {
-            return <ColourContainer id={id} colour={colour} key={id} />;
+          {Object.entries(colours).map(([key, data]) => {
+            return <ColourContainer id={key} colour={data} key={key} />;
           })}
         </div>
       </DragDropContext>
--- src/initialData.ts
+++ src/initialData.ts
@@ -1,10 +1,17 @@
-import { ColourData } from "./interfaces";
+import { ColourData, PegInputs } from "./interfaces";
 
 export const colourData: ColourData = {
   redOrigin: {
     hsl: "hsl(356, 100%, 41%)",
+    id: "redOrigin",
   },
   blueOrigin: {
     hsl: "hsl(226, 65%, 35%)",
+    id: "blueOrigin",
   },
+};
+
+export const pegInputsData: PegInputs = {
+  one: { id: "one", peg: [] },
+  two: { id: "two", peg: [] },
 };
