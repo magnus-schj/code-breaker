5cde9549b5bd69fb854bcd5c9f6027abb7c996d8
Magnus <beachmagnus+git@gmail.com>
2021-10-28 11:53:19 +0200

    actuall functioning game

--- src/components/Attempts.component.tsx
+++ src/components/Attempts.component.tsx
@@ -1,11 +1,41 @@
 import React, { FC } from "react";
 import { useAppSelector } from "../App/hooks";
+import { Attempt } from "../interfaces";
 
 interface Props {}
 
 const Attempts: FC<Props> = () => {
   const code = useAppSelector((state) => state.code);
-  return <div></div>;
+  const renderPegs = (num: number, isBlack: boolean) => {
+    const pegs = [];
+    for (let i = 0; i < num; i++) {
+      const element = (
+        <div
+          style={{
+            height: "0.5rem",
+            width: "0.5rem",
+            border: "1px solid black",
+            background: isBlack ? "black" : "white",
+          }}
+        ></div>
+      );
+      pegs.push(element);
+    }
+    return pegs;
+  };
+  return (
+    <div>
+      {code.attempts.map(({ black, white }, i) => (
+        <div className="attempt">
+          <ul>Attempt number {i + 1}</ul>
+          <div className="attempt-pegs">
+            {renderPegs(black, true)}
+            {renderPegs(white, false)}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Attempts;
