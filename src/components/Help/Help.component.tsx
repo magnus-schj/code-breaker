6cdd1475e5021866d4c98ef2f2126cb60e2c20ea
Magnus <beachmagnus+git@gmail.com>
2021-11-25 21:44:06 +0100

    close icon ok

--- src/components/Help/Help.component.tsx
+++ src/components/Help/Help.component.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
 import "./Help.styles.css";
 import { motion, Variants } from "framer-motion";
-import { Card, CardContent, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { Card, CardContent, IconButton, Typography } from "@mui/material";
 
 const dropIn: Variants = {
   hidden: {
@@ -36,6 +37,7 @@ const HelpComponent: FC<Props> = ({ handleClose }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
+      style={{ zIndex: 9001 }}
     >
       <motion.div
         onClick={(e) => e.stopPropagation()}
@@ -46,9 +48,14 @@ const HelpComponent: FC<Props> = ({ handleClose }) => {
       >
         <Card style={{ padding: "1rem", margin: "1rem" }}>
           <CardContent>
-            <Typography variant="h3" color="initial" gutterBottom>
-              Help
-            </Typography>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography variant="h3" color="initial" gutterBottom>
+                Help
+              </Typography>
+              <IconButton onClick={handleClose}>
+                <CloseIcon />
+              </IconButton>
+            </div>
             <Typography variant="body1" color="initial">
               Place colours in the emoty boxes, and press submit. The attempt
               will be displayed, with the colour combination, how many correct
